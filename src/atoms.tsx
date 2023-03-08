47ff59df66612959ed46219aaed2a3e7cbb06a20
hihiihiii <ahrthfla@naver.com>
2023-03-09 01:19:16 +0900

    local storage 기능 수정

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -5,70 +5,19 @@ export interface ITodo {
   text: string;
 }
 
-export interface IBoard {
-  id: number;
-  title: string;
-  toDos: ITodo[];
-}
-
 interface ITodoState {
   [key: string]: ITodo[];
 }
-
-const instanceOfToDo = (object: unknown): object is ITodo => {
-  return (
-    object !== null &&
-    object !== undefined &&
-    object.constructor === Object &&
-    typeof (object as { id: unknown; text: unknown }).id === "number" &&
-    typeof (object as { id: unknown; text: unknown }).text === "string"
-  );
-};
-
-const instanceOfBoard = (object: unknown): object is IBoard => {
-  return (
-    object !== null &&
-    object !== undefined &&
-    object.constructor === Object &&
-    typeof (object as { id: unknown; title: unknown; toDos: unknown }).id ===
-      "number" &&
-    typeof (object as { id: unknown; title: unknown; toDos: unknown }).title ===
-      "string" &&
-    Array.isArray(
-      (object as { id: unknown; title: unknown; toDos: unknown }).toDos
-    ) &&
-    (object as { id: unknown; title: unknown; toDos: unknown[] }).toDos.every(
-      (toDo) => instanceOfToDo(toDo)
-    )
-  );
-};
-
-const instanceOfBoards = (object: unknown): object is IBoard[] => {
-  return (
-    Array.isArray(object) && object.every((board) => instanceOfBoard(board))
-  );
-};
 
 const localStorageEffect =
   (key: string) =>
   ({ setSelf, onSet }: any) => {
     const savedValue = localStorage.getItem(key);
-
-    if (savedValue !== null && savedValue !== undefined) {
-      const json = (raw: string) => {
-        try {
-          return JSON.parse(raw);
-        } catch (error) {
-          return false;
-        }
-      };
-
-      if (json(savedValue) && instanceOfBoards(json(savedValue))) {
-        setSelf(json(savedValue));
-      }
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
     }
-    onSet((newValue: IBoard[]) => {
-      localStorage.setItem(key, JSON.stringify(newValue));
+    onSet((newValue: ITodo[]) => {
+      localStorage.setItem("trello", JSON.stringify(newValue));
     });
   };
 
